d16970f1f3e0f54ca32d98b5846f41c32410e239
andyparla <andyparla@gmail.com>
2021-07-06 15:46:59 +0200

    mejora clase botonera

--- ButtonManager/buttonManager.py
+++ ButtonManager/buttonManager.py
@@ -2,27 +2,22 @@ import RPi.GPIO as GPIO
 import signal
 import sys
 
-GPIO.setmode(GPIO.BCM)
+class Botonera():
 
-BUTTON_GPIO = 4;  # button to monitor for button presses.
-LED_output = 17;  # LED to light or not depending on button presses.
+    BUTTON_GPIO = 4;  # Boton1 para grabar.
 
-# GPIO btn_input set up as input.
-GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# GPIO.setup(LED_output, GPIO.OUT)
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.BUTTON_GPIO,
+                              GPIO.BOTH,
+                              callback=lambda x: self.button_callback(self.BUTTON_GPIO))
 
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
+        #SI TUVIERAMOS UN LED PARA MOSTRAR QUE ESTA GRABANDO
+        # GPIO.setup(LED_output, GPIO.OUT)
 
-def button_callback(channel):
-    if not GPIO.input(BUTTON_GPIO):
-        print("Button pressed!")
-    else:
-        print("Button released!")
-
-
-GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH, callback=button_callback)
-
-signal.signal(signal.SIGINT, signal_handler)
-signal.pause()
+    def button_callback(self, numButton):
+        if not GPIO.input(self.BUTTON_GPIO):
+            print(f"Button pressed {str(numButton)}")
+        else:
+            print(f"Button released {str(numButton)}")

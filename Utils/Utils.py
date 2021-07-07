9852cdca8b8f059181b1876b160b38f7e971f529
andyparla <andyparla@gmail.com>
2021-07-07 16:15:03 +0200

    obtenida clave

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -4,13 +4,19 @@ class UtilsEncrypt():
 
     fernet = None
     key = None
-    def __init__(self):
-        self.fernet = Fernet(Fernet.generate_key())
+    def __init__(self, key):
+        if key != None:
+            self.key = key
+        else:
+            self.key = Fernet.generate_key()
+            print(self.key)
+
+        self.fernet = Fernet(self.key)
 
     def encryptData(self, data):
         return self.fernet.encrypt(data.encode())
     def decryptData(self, data):
         return self.fernet.decrypt(data).decode()
-util = UtilsEncrypt()
+util = UtilsEncrypt(b'DPYVeSnMa_rcAwrtBcn2dJpfuzz4R4f3Ax1HIv5Gi8c=')
 # print(util.encryptData("1856142691:AAFvGIIKkvPsrivlAnL9CRr272xJl-yKi80"))
-print(util.decryptData("gAAAAABg5bTDpaFN2f4kC971kEw3RYewlfYrxhAPBwAQTo7JbLPsm1m1C5UEdQZ8Kwc4sy2NIUh-TV6KdCLo6rpy17TaJHzKc0TJaJATjlGcru4aRINEafSLKXKu8sSxfcGahAIYErHQ"))
+print(util.decryptData(b'gAAAAABg5baxxikQjdQixPTSrhOGCN9CjsBo_NJvTbAmMEVT8EL1TjZAN0aM0VWSFU4A7EgwM6jKqZBVt27dz5O1O2_73JA0MtxiUEM6RVKUbPZZ5MseQE29pBnX0mvSQ0TDvE-ydlVE'))

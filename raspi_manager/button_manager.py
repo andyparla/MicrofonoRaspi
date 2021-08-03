99dec576c86baa573c31abda91cf4585ffc63c7f
ATSISTEMAS\apuerto <apuerto@atsistemas.com>
2021-08-03 16:35:06 +0200

    pruebas

--- raspi_manager/button_manager.py
+++ raspi_manager/button_manager.py
@@ -56,6 +56,5 @@ class ButtonManager():
             print(f"Boton liberado {str(boton.pin.number)}")
             self.microfono_start_audio.NOMBRE_BOTON = nombre_boton
             self.microfono_start_audio.GRABAR_AUDIO = True
-
-            # fichero_audio = self.microfono_start_audio.parar_grabacion(nombre_boton)
-            # self.telebotClass.enviar_audio(fichero_audio)
+            fichero_audio = self.microfono_start_audio.parar_grabacion(nombre_boton)
+            self.telebotClass.enviar_audio(fichero_audio)
